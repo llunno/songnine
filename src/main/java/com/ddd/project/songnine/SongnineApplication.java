f9705a3407a25f3165a223ca2977569f84be7ec8
Lucas Nogueira <lucasjesusnogueira@gmail.com>
2024-06-21 11:44:52 +0000

    base commit

---
+++ src/main/java/com/ddd/project/songnine/SongnineApplication.java
@@ -0,0 +1,13 @@
+package com.ddd.project.songnine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongnineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongnineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddd/project/songnine/SongnineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddd.project.songnine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongnineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
